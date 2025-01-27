9a41683784550cb3298314166b3cd91364f9d888
Giselle <cryponiess@gmail.com>
2025-01-28 09:08:25 +1100

    added basic components for auth page (still unstyled)

---
+++ frontend/src/components/AuthButton.tsx
@@ -0,0 +1,10 @@
+/* TODO: use props to customize the text button */
+const AuthButton = () => {
+  return (
+    <>
+      <button>MODIFY ME</button>
+    </>
+  );
+};
+
+export { AuthButton };
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,5 +1,21 @@
+import Logo from "../assets/Logo.png";
+import { Input } from "../components/Input";
+import { AuthButton } from "../components/AuthButton";
+
 const LoginPage = () => {
-  return <>Login</>;
+  return (
+    <>
+      <img src={Logo}></img>
+      <div id="login-box">
+        <h2>Login</h2>
+        <Input text="Email" />
+        <Input text="Password" />
+        <a>Forgot password?</a>
+        <AuthButton />
+        <p>Don’t have an account? Register</p>
+      </div>
+    </>
+  );
 };
 
 export { LoginPage };
