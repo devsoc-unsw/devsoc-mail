95068dca7e67aaa29ce70a64bd3244b2fd9b3674
alyssacheong <106944368+alyssacheong@users.noreply.github.com>
2025-01-29 08:54:16 +1100

    added logo to mail page

--- frontend/src/pages/MailPage.tsx
+++ frontend/src/pages/MailPage.tsx
@@ -1,9 +1,22 @@
+import Logo from "../assets/Logo.png";
+import { Input } from "../components/Input";
 const MailPage = () => {
-  return <>
-  <p>mail</p>
-  
-  
-  </>;
+  return (
+    <>
+      <img
+        src={Logo}
+        style={{
+          width: "10%",
+          height: "auto",
+          position: "absolute",
+          top: "0",
+          left: "0",
+        }}
+      ></img>
+        <Input text="Search" />
+      <p>mail</p>
+    </>
+  );
 };
 
 export { MailPage };
