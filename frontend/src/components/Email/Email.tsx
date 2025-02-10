27e2c2a21ee4d9aa24037d167e5964a28dcff595
alyssacheong <106944368+alyssacheong@users.noreply.github.com>
2025-02-11 01:04:07 +1100

    added delete all feature

--- frontend/src/components/Email/Email.tsx
+++ frontend/src/components/Email/Email.tsx
@@ -3,32 +3,50 @@ import styles from "./Email.module.css";
 import { Checkbox } from "@mui/material";
 
 interface EmailProps {
+  id: number;
   subject: string;
   date: string;
   from: string;
   to: string[];
   body: string;
   page: string;
+  setSelectedEmails: React.Dispatch<React.SetStateAction<number[]>>;
+  selectedEmails: number[];
 }
 
 const Email = (props: EmailProps) => {
-  const navigate = useNavigate();
-  return (
-    <div className={styles.emailBox}>
-      <Checkbox></Checkbox>
-      <button
-        onClick={() => {
-          navigate(props.page);
-        }}
-        className={styles.emailButton}
-      >
-        <p className={styles.emailFrom}>{props.from}</p>
-        <p className={styles.emailSubject}>{props.subject}</p>
-        <p>{props.body}</p>
-        <p>{props.date}</p>
-      </button>
-    </div>
-  );
-};
+    const navigate = useNavigate();
+  
+    // when checkbox selected, email id added to an array of email ids to be deleted
+    // when checkbox unselected, remove this email id from the array
+    const selectEmail = (emailId: number) => {
+      props.setSelectedEmails((prev) =>
+        prev.includes(emailId)
+          ? prev.filter((id) => id !== emailId)
+          : [...prev, emailId]
+      );
+    };
+  
+    return (
+      <div className={styles.emailBox}>
+        <Checkbox
+          checked={props.selectedEmails.includes(props.id)}
+          onChange={() => selectEmail(props.id)}
+        />
+        <button
+          onClick={() => {
+            navigate(props.page);
+          }}
+          className={styles.emailButton}
+        >
+          <p className={styles.emailFrom}>{props.from}</p>
+          <p className={styles.emailSubject}>{props.subject}</p>
+          <p>{props.body}</p>
+          <p>{props.date}</p>
+        </button>
+      </div>
+    );
+  };
+  
 
 export { Email };
--- frontend/src/pages/MailPage/MailPage.tsx
+++ frontend/src/pages/MailPage/MailPage.tsx
@@ -4,20 +4,63 @@ import { Email } from "../../components/Email/Email";
 import styles from "./MailPage.module.css";
 import { Composebutton } from "../../components/ComposeButton";
 import { IconButton } from "@mui/material";
-import LogoutIcon from '@mui/icons-material/Logout';
+import LogoutIcon from "@mui/icons-material/Logout";
 import { useNavigate } from "react-router-dom";
-
+import { useState } from "react";
 
 const MailPage = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [selectedEmails, setSelectedEmails] = useState<number[]>([]);
 
+  const [emails, setEmails] = useState([
+    {
+      id: 1,
+      subject: "Dummy",
+      date: "26 Jan 2025, 17:12",
+      from: "admin@devsoc.mail",
+      to: ["me@devsoc.mail", "you@devsoc.mail"],
+      body: "Lorem ipsum odor amet... ",
+      page: "/view",
+    },
+    {
+      id: 2,
+      subject: "Dummy",
+      date: "26 Jan 2025, 17:12",
+      from: "admin@devsoc.mail",
+      to: ["me@devsoc.mail", "you@devsoc.mail"],
+      body: "Lorem ipsum odor amet... ",
+      page: "/view",
+    },
+    {
+      id: 3,
+      subject: "Dummy",
+      date: "26 Jan 2025, 17:12",
+      from: "admin@devsoc.mail",
+      to: ["me@devsoc.mail", "you@devsoc.mail"],
+      body: "Lorem ipsum odor amet... ",
+      page: "/view",
+    },
+  ]);
+
+  const deleteEmails = () => {
+    console.log("delete");
+    const updatedEmails = emails.filter(
+      (email) => !selectedEmails.includes(email.id)
+    );
+
+    setEmails(updatedEmails);
+  };
 
   return (
     <>
-    <IconButton onClick={() => {navigate("/")}}>
+      <IconButton
+        onClick={() => {
+          navigate("/");
+        }}
+      >
         <LogoutIcon></LogoutIcon>
-    </IconButton>
-      <button>Delete All</button>
+      </IconButton>
+      <button onClick={deleteEmails}>Delete All</button>
       <Composebutton />
       <img src={Logo} className={styles.devsocLogo}></img>
       <div className={styles.searchBar}>
@@ -25,30 +68,19 @@ const MailPage = () => {
       </div>
 
       <div className="email">
-        <Email
-          subject="Dummy"
-          date="26 Jan 2025, 17:12"
-          from="admin@devsoc.mail"
-          to={["me@devsoc.mail", "you@devsoc.mail"]}
-          body="Lorem ipsum odor amet... "
-          page="/view"
-        />
-        <Email
-          subject="Dummy"
-          date="26 Jan 2025, 17:12"
-          from="admin@devsoc.mail"
-          to={["me@devsoc.mail", "you@devsoc.mail"]}
-          body="Lorem ipsum odor amet... "
-          page="/view"
-        ></Email>
-        <Email
-          subject="Dummy"
-          date="26 Jan 2025, 17:12"
-          from="admin@devsoc.mail"
-          to={["me@devsoc.mail", "you@devsoc.mail"]}
-          body="Lorem ipsum odor amet... "
-          page="/view"
-        ></Email>
+        {emails.map((email) => (
+          <Email
+            id={email.id}
+            subject={email.subject}
+            date={email.date}
+            from={email.from}
+            to={email.to}
+            body={email.body}
+            page={email.page}
+            setSelectedEmails={setSelectedEmails}
+            selectedEmails={selectedEmails}
+          ></Email>
+        ))}
       </div>
     </>
   );
