54ea5f993ce583b0204f2adf25bad5de36486fde
alyssacheong <106944368+alyssacheong@users.noreply.github.com>
2025-03-08 16:25:40 +1100

    commented out react logic

--- frontend/src/pages/MailPage/MailPage.tsx
+++ frontend/src/pages/MailPage/MailPage.tsx
@@ -1,55 +1,54 @@
 import Logo from "../../assets/Logo.png";
-import { Input } from "../../components/Input/Input";
-import { Email } from "../../components/Email/Email";
+// import { Email } from "../../components/Email/Email";
 import styles from "./MailPage.module.css";
 import { Composebutton } from "../../components/ComposeButton";
 import { IconButton } from "@mui/material";
 import LogoutIcon from "@mui/icons-material/Logout";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+// import { useState } from "react";
 
 const MailPage = () => {
   const navigate = useNavigate();
-  const [selectedEmails, setSelectedEmails] = useState<number[]>([]);
+//   const [selectedEmails, setSelectedEmails] = useState<number[]>([]);
 
-  const [emails, setEmails] = useState([
-    {
-      id: 1,
-      subject: "Dummy",
-      date: "26 Jan 2025, 17:12",
-      from: "admin@devsoc.mail",
-      to: ["me@devsoc.mail", "you@devsoc.mail"],
-      body: "Lorem ipsum odor amet... ",
-      page: "/view",
-    },
-    {
-      id: 2,
-      subject: "Dummy",
-      date: "26 Jan 2025, 17:12",
-      from: "admin@devsoc.mail",
-      to: ["me@devsoc.mail", "you@devsoc.mail"],
-      body: "Lorem ipsum odor amet... ",
-      page: "/view",
-    },
-    {
-      id: 3,
-      subject: "Dummy",
-      date: "26 Jan 2025, 17:12",
-      from: "admin@devsoc.mail",
-      to: ["me@devsoc.mail", "you@devsoc.mail"],
-      body: "Lorem ipsum odor amet... ",
-      page: "/view",
-    },
-  ]);
+//   const [emails, setEmails] = useState([
+//     {
+//       id: 1,
+//       subject: "Dummy",
+//       date: "26 Jan 2025, 17:12",
+//       from: "admin@devsoc.mail",
+//       to: ["me@devsoc.mail", "you@devsoc.mail"],
+//       body: "Lorem ipsum odor amet... ",
+//       page: "/view",
+//     },
+//     {
+//       id: 2,
+//       subject: "Dummy",
+//       date: "26 Jan 2025, 17:12",
+//       from: "admin@devsoc.mail",
+//       to: ["me@devsoc.mail", "you@devsoc.mail"],
+//       body: "Lorem ipsum odor amet... ",
+//       page: "/view",
+//     },
+//     {
+//       id: 3,
+//       subject: "Dummy",
+//       date: "26 Jan 2025, 17:12",
+//       from: "admin@devsoc.mail",
+//       to: ["me@devsoc.mail", "you@devsoc.mail"],
+//       body: "Lorem ipsum odor amet... ",
+//       page: "/view",
+//     },
+//   ]);
 
-  const deleteEmails = () => {
-    console.log("delete");
-    const updatedEmails = emails.filter(
-      (email) => !selectedEmails.includes(email.id)
-    );
+//   const deleteEmails = () => {
+//     console.log("delete");
+//     const updatedEmails = emails.filter(
+//       (email) => !selectedEmails.includes(email.id)
+//     );
 
-    setEmails(updatedEmails);
-  };
+//     setEmails(updatedEmails);
+//   };
 
   return (
     <>
@@ -60,24 +59,10 @@ const MailPage = () => {
       >
         <LogoutIcon></LogoutIcon>
       </IconButton>
-      <button onClick={deleteEmails}>Delete All</button>
+      <button>Delete All</button>
       <Composebutton />
       <img src={Logo} className={styles.devsocLogo}></img>
-      <div className="email">
-        {emails.map((email) => (
-          <Email
-            id={email.id}
-            subject={email.subject}
-            date={email.date}
-            from={email.from}
-            to={email.to}
-            body={email.body}
-            page={email.page}
-            setSelectedEmails={setSelectedEmails}
-            selectedEmails={selectedEmails}
-          ></Email>
-        ))}
-      </div>
+      
     </>
   );
 };
