14615d4f48afd48d9045b37462ffe2ef834f26bc
Kim Lam <kim.lam19442@gmail.com>
2025-03-13 22:03:59 +1100

    started small style work on login page

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -4,17 +4,17 @@ import { AuthButton } from "../components/AuthButton/AuthButton";
 
 const LoginPage = () => {
   return (
-    <>
-      <img src={Logo}></img>
-      <form id="login-box">
-        <h2>Login</h2>
+    <div className="max-w-wd flex flex-col items-center justify-center min-h-screen p-4 bg-white">
+      <img src={Logo} className="mb-4"></img>
+      <form id="login-box" className="max-w-wd p-6 bg-white border border-black-300 rounded-lg shadow-md">
+        <h2 className="text-xl font-semibold text-center mb-4">Login</h2>
         <Input text="Email" />
         <Input text="Password" />
-        <a>Forgot password?</a>
+        <a className="text-sm text-black-600 hover:underline">Forgot password?</a>
         <AuthButton to="/mail" />
         <p>Don’t have an account? Register</p>
       </form>
-    </>
+    </div>
   );
 };
 
