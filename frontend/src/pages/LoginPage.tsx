a613150abea650fd543e239e64f4631169367877
Alfie728 <zegro728@gmail.com>
2025-03-14 21:18:20 +1100

    cleaned up LoginPage for demo purpose

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -3,21 +3,7 @@ import { Input } from "../components/Input";
 import { AuthButton } from "../components/AuthButton";
 
 const LoginPage = () => {
-  return (
-    <div>
-      <img src={Logo} />
-      <form>
-        <h2>Login</h2>
-        <Input placeholder="Email" />
-        <Input placeholder="Password" />
-        <a>Forgot password?</a>
-        <AuthButton to="/mail" text="Login" />
-        <p>
-          Don't have an account? <span>Register</span>
-        </p>
-      </form>
-    </div>
-  );
+  return <div>Live coding session!!</div>;
 };
 
 export { LoginPage };
