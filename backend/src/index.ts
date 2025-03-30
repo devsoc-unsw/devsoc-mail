438d72bb74a725c006d4b081ce97c756b76cdff5
Giselle Angela Hanjaya Putri <z5491414@ad.unsw.edu.au>
2025-03-30 18:45:47 +1100

    added cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import mailRoutes from './routes/mail.routes';
 // import { dbConnect, dbDisconnect } from "./db";
 import { PORT } from "../config.json"
 import { errorMiddleware } from "./middleware";
+import cors from "cors";
 
 dotenv.config();
 
@@ -21,6 +22,7 @@ app.listen(port, () => {
 
 // routes & middleware
 app.use(express.json());
+app.use(cors());
 app.use('', authRoutes);
 app.use('', mailRoutes);
 app.use(errorMiddleware);
