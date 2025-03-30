3b206eee15e799680d18aa4f5c547dceeb7820e2
lachlanshoesmith <12870244+lachlanshoesmith@users.noreply.github.com>
2025-03-30 13:55:06 +1100

    🛫 solve task 1 and task 2

--- frontend/src/components/Input/Input.tsx
+++ frontend/src/components/Input/Input.tsx
@@ -1,11 +1,17 @@
+import React from 'react';
+
 type InputProp = {
   text?: string;
+  setter: React.Dispatch<React.SetStateAction<string>>;
 };
 
 const Input = (props: InputProp) => {
   return (
     <>
-      <input placeholder={props.text}></input>
+      <input
+        placeholder={props.text}
+        onChange={(e) => props.setter(e.target.value)}
+      ></input>
     </>
   );
 };
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,16 +1,20 @@
-import Logo from "../assets/Logo.png";
-import { Input } from "../components/Input/Input";
-import { AuthButton } from "../components/AuthButton/AuthButton";
-import { Link } from "react-router-dom";
+import Logo from '../assets/Logo.png';
+import { Input } from '../components/Input/Input';
+import { AuthButton } from '../components/AuthButton/AuthButton';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [_, setPassword] = useState('');
   return (
     <>
       <img src={Logo}></img>
       <form id="login-box">
         <h2>Login</h2>
-        <Input text="Email" />
-        <Input text="Password" />
+        <Input text="Email" setter={setEmail} />
+        {email && <p>The email entered is {email}.</p>}
+        <Input text="Password" setter={setPassword} />
         <a>Forgot password?</a>
         <AuthButton to="/mail">Log in</AuthButton>
         <p>
