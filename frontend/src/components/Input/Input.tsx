576f29027ff2190780e54fe2577d776b91e9ab62
lachlanshoesmith <12870244+lachlanshoesmith@users.noreply.github.com>
2025-03-30 14:31:28 +1100

    🫡 complete all tasks

--- frontend/src/components/Input/Input.tsx
+++ frontend/src/components/Input/Input.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 type InputProp = {
   text?: string;
   setter: React.Dispatch<React.SetStateAction<string>>;
+  onChange?: () => void; // needed for task 6
 };
 
 const Input = (props: InputProp) => {
@@ -10,7 +11,11 @@ const Input = (props: InputProp) => {
     <>
       <input
         placeholder={props.text}
-        onChange={(e) => props.setter(e.target.value)}
+        onChange={(e) => {
+          props.setter(e.target.value);
+          // task 6 solution
+          if (props.onChange) props.onChange();
+        }}
       ></input>
     </>
   );
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -2,11 +2,17 @@ import Logo from '../assets/Logo.png';
 import { Input } from '../components/Input/Input';
 import { AuthButton } from '../components/AuthButton/AuthButton';
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const LoginPage = () => {
   const [email, setEmail] = useState('');
-  const [_, setPassword] = useState('');
+  const [password, setPassword] = useState('');
+  useEffect(() => {
+    const userDataObject = localStorage.getItem('userData');
+    if (userDataObject != null) {
+      alert('User is already logged in.');
+    }
+  }, []);
   return (
     <>
       <img src={Logo}></img>
@@ -16,7 +22,20 @@ const LoginPage = () => {
         {email && <p>The email entered is {email}.</p>}
         <Input text="Password" setter={setPassword} />
         <a>Forgot password?</a>
-        <AuthButton to="/mail">Log in</AuthButton>
+        <AuthButton
+          onClick={() => {
+            const data = {
+              email,
+              password,
+              loginTime: new Date(),
+            };
+            const dataString = JSON.stringify(data);
+            localStorage.setItem('userData', dataString);
+          }}
+          to="/mail"
+        >
+          Log in
+        </AuthButton>
         <p>
           Don’t have an account? <Link to="/register">Register</Link>
         </p>
