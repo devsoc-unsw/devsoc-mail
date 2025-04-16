57d8b451de1ed474d3fd1b4ed7c9c042c6258e36
Kim Lam <kim.lam19442@gmail.com>
2025-04-16 22:43:16 +1000

    made comments for db.ts

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -13,59 +13,22 @@ export let dataCollection: Collection;
 
 export async function connectToDatabase() {
   try {
-    dotenv.config();
-    const atlasUri = process.env.ATLAS_URI;
-    if (!atlasUri) {
-      throw new Error("ATLAS_URI environment variable is undefined");
-    }
 
-    client = new MongoClient(atlasUri);
-    await client.connect();
 
-    db = client.db();
-    console.log(`Successfully connected to database: ${db.databaseName}`);
+    // MAKE SURE TO FOLLOW ALONG DURING DEMO - TO FILL OUT
 
-    // Initialize collections
-    sessionsCollection = db.collection("sessions");
-    usersCollection = db.collection("users");
-    mailsCollection = db.collection("mail");
-    dataCollection = db.collection("data");
 
-    // Initialize collections if they don't exist
-    const usersCount = await usersCollection.countDocuments();
-    if (usersCount === 0) {
-      await usersCollection.insertOne({
-        users: [],
-      });
-    }
 
-    const mailsCount = await mailsCollection.countDocuments();
-    if (mailsCount === 0) {
-      await mailsCollection.insertOne({
-        mails: [],
-      });
-    }
-
-    const sessionsCount = await sessionsCollection.countDocuments();
-    if (sessionsCount === 0) {
-      await sessionsCollection.insertOne({
-        sessions: [],
-      });
-    }
-    const dataCount = await dataCollection.countDocuments();
-    if (dataCount === 0) {
-      await dataCollection.insertOne({
-        data: [],
-      });
-    }
   } catch (error) {
     console.error("Error found when connecting to MongoDB: ", error);
   }
 }
 
 export async function closeDatabaseConnection() {
-  if (client) {
-    await client.close();
-    console.log("Database connection closed");
-  }
+  // uncomment once connectToDatabase is written
+  
+  // if (client) {
+  //   await client.close();
+  //   console.log("Database connection closed");
+  // }
 }
