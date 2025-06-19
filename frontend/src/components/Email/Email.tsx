bed40dcd6c9e96ec25992c93592d8c1c9238e96e
alyssacheong <106944368+alyssacheong@users.noreply.github.com>
2025-06-19 10:16:41 +1000

    added stuff

--- frontend/src/components/Email/Email.tsx
+++ frontend/src/components/Email/Email.tsx
@@ -3,3 +3,42 @@
  * 
  * Make an email component here
  */
+
+import { useNavigate } from "react-router-dom";
+import styles from "./Email.module.css";
+import { Checkbox } from "@mui/material";
+
+interface EmailProps {
+  id: number;
+  subject: string;
+  date: string;
+  from: string;
+  to: string[];
+  body: string;
+  page: string;
+}
+
+const Email = (props: EmailProps) => {
+    const navigate = useNavigate();
+  
+    return (
+      <div className={styles.emailBox}>
+        <Checkbox
+        />
+        <button
+          onClick={() => {
+            navigate(props.page);
+          }}
+          className={styles.emailButton}
+        >
+          <p className={styles.emailFrom}>{props.from}</p>
+          <p className={styles.emailSubject}>{props.subject}</p>
+          <p>{props.body}</p>
+          <p>{props.date}</p>
+        </button>
+      </div>
+    );
+  };
+
+export { Email };
+
--- frontend/src/pages/MailPage/MailPage.tsx
+++ frontend/src/pages/MailPage/MailPage.tsx
@@ -3,7 +3,7 @@ import styles from "./MailPage.module.css";
 import { Composebutton } from "../../components/ComposeButton";
 import { IconButton } from "@mui/material";
 import LogoutIcon from "@mui/icons-material/Logout";
-
+import {Email} from "../../components/Email/Email";
 
 /**
  * Workshop 2: Routes exercise
@@ -13,33 +13,37 @@ import LogoutIcon from "@mui/icons-material/Logout";
  * 1. Run `npm install react-router-dom`
  * 2. Go to main.tsx file and check if app component is wrapped with BrowseRouter
  * 3. Go to App.tsx file and add route. (instructions are there)
- * 4. Go to Email.tsx and import useNavigate from react-router-dom. 
+ * 4. Go to Email.tsx and import useNavigate from react-router-dom.
  * 5. Add an email to MailPage.tsx and make it navigate to the ViewPage when clicked.
  * 6. Add navigate logic to the logout button
  */
 
 const MailPage = () => {
-  
-
   return (
     <>
       <IconButton
-        // Step 6: Add logic to make this button navigate to logout screen.
+      // Step 6: Add logic to make this button navigate to logout screen.
       >
         <LogoutIcon></LogoutIcon>
       </IconButton>
-      
 
       <button>Delete All</button>
       <Composebutton />
-       
-       {/* Step 5: Add an email here!!! */}
-      
+
+      {/* Step 5: Add an email here!!! */}
+
+      <Email
+        id={1}
+        subject="Hello"
+        date="2025-06-19"
+        from="alyssa@devsoc.mail"
+        to={["alyssa@devsoc.mail"]}
+        body="This is a test email"
+        page="/view"
+      />
       <img src={Logo} className={styles.devsocLogo}></img>
     </>
   );
 };
 
 export { MailPage };
-
-
