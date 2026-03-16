5037c809b29c30724a15d39c984323b0661e9dae
AndyAtmadja03S <atmandy345@gmail.com>
2026-03-17 00:44:26 +1100

    added new page

--- frontend/src/pages/answer.tsx
+++ frontend/src/pages/answer.tsx
@@ -51,31 +51,34 @@ const MailPageGrid = () => {
 
 
   return (
-     <main className="max-w-4xl mx-auto p-4 bg-white flex flex-col">
-      <nav>
-        <img src={Logo} />
+    <main className="max-w-4xl mx-auto p-4 bg-white flex flex-col">
+      <nav className="flex justify-between items-center">
+        <img src={Logo} className="h-10" />
         <Input
           placeholder="Search"
+          className="w-1/2 border-2 border-black"
           setter={setSearchTerm}
         />
         <Button
           onClick={() => navigate("/")}
+          className="bg-[#D9807E] border-2 border-black p-2 rounded"
         >
           Logout
         </Button>
       </nav>
 
-			<div className="border-black border-2 rounded-lg mt-4">
-        <div>
+      <div className="border-black border-2 rounded-lg mt-4">
+        <div className="mt-4 mb-4 flex justify-between px-4">
           <Button
+            className="cursor-pointer text-white bg-[#D34B48] border-2 border-black p-2 rounded"
             onClick={deleteEmails}
           >
             Delete All
           </Button>
-          <ComposeButton/>
+          <ComposeButton className="bg-[#5DAB61] border-2 border-black p-2 rounded" />
         </div>
 
-        <div className="">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
           {emails.map((email) => (
             <EmailCard
               key={email.id}
